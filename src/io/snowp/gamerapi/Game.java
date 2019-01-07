2e0e0d68956279ebbadaca772f68e874f68f99df
SnowpMakes <30631656+SnowpMakes@users.noreply.github.com>
2019-01-07 07:24:39 +0100

    Added Game interface

---
+++ src/io/snowp/gamerapi/Game.java
@@ -0,0 +1,28 @@
+/*
+ * This file is part of GamerAPI. Licenced under the GNU General Public Licence version 3
+ *
+ * GamerAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GamerAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GamerAPI.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) SnowpMakes
+ * Copyright (c) contributors
+ */
+
+package io.snowp.gamerapi;
+
+/*
+ * All Games extend this interface
+ */
+public interface Game
+{
+}
